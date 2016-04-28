ae670815b5d6740f50e6811a435f92f1f0156762
Joshua Zink-Duda <zinkdudajoshua@gmail.com>
2016-04-28 10:44:45 -0500

    updated to be general + error handling

--- TwitterQuery.py
+++ TwitterQuery.py
@@ -4,54 +4,90 @@ from textblob import TextBlob
 import sys
 sys.path.append("../")
 from twitter_credentials import *
+import time
 '''
-example for analyzing tweets about the cast of The Avengers
+Gets twitter analysis of first 5 members of cast and director
 
 '''
+MAX_TWEETS=100
 
-auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
-auth.set_access_token(access_token, access_token_secret)
+def get_twitter_imdb_access():
+    auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
+    auth.set_access_token(access_token, access_token_secret)
+    
+    api = tweepy.API(auth)
+    
+    ia = imdb.IMDb()
+    return api,ia
 
-api = tweepy.API(auth)
-
-ia = imdb.IMDb()
-
-s_result = ia.search_movie("Avengers")
-
-aveng = s_result[0]
-ia.update(aveng)
-
-def tweet_stats(tweets):
-    count,pol,sub = 0,0.0,0.0
+def tweet_stats(tweets,count):
+    pol,sub = 0.0,0.0
     for t in tweets:
         if t.text[0:2] <> 'RT':
             blob = TextBlob(t.text)
             count += 1
             pol += blob.polarity
             sub += blob.subjectivity
-            
+        if count == MAX_TWEETS: break
+
     return count,pol,sub
 
+def get_tweets(api,name,max_id=None):
+    ratelimit = True
+    tweets = None
+    while ratelimit:
+        try:
+            if max_id == None:
+                tweets = api.search(q=name,lang='en',count=100)
+            else:
+                tweets = api.search(q=name,lang='en',count=100,max_id=max_id)
+            ratelimit = False
+        except tweepy.error.RateLimitError:
+            print 'RateLimitError, trying again in 60 seconds'
+            time.sleep(60)
+    return tweets
 
-for actor in aveng['cast']:
-    name = actor['name']
+def twitter_search(movie,api,ia):
+    
+    s_result = ia.search_movie(movie)
 
-    count,pol,sub = 0,0.0,0.0
-    for p in range(3):
-        tweets = None
-        if p == 0:
-            tweets = api.search(q=name,lang='en',count=100)
+    mov = s_result[0]
+    ia.update(mov)
+
+    result = []
+    for i in range(2):
+        name = ''
+        if i == 0:
+            name = mov['cast'][0]['name']
         else:
-            tweets = api.search(q=name,lang='en',count=100,max_id=max_id)
-        stats = tweet_stats(tweets)
-        count += stats[0]
-        pol += stats[1]
-        sub += stats[2]
-        max_id = tweets[-1].id
+            name = mov['director'][0]['name']
 
-    if count == 0: break
-    if count < 25: break
-    print name
-    print 'Polarity:\t',pol/count
-    print 'Subjectivity:\t',sub/count
-    print 'Count:\t\t',count
+        count,pol,sub = 0,0.0,0.0
+        max_id = None
+        for p in range(10):
+            tweets = get_tweets(api,name,max_id)
+            stats = tweet_stats(tweets,count)
+            count = stats[0]
+            pol += stats[1]
+            sub += stats[2]
+            max_id = tweets[-1].id
+            if count == MAX_TWEETS: break
+
+        if count < MAX_TWEETS:
+            print name,count
+            break
+
+        result.append((pol,sub))
+        print name
+        print 'Polarity:\t',pol
+        print 'Subjectivity:\t',sub
+        print 'Count:\t\t',count
+
+    return result
+
+
+if __name__ == '__main__':
+    api,ia = get_twitter_imdb_access()
+    movies = ['The Avengers','Batman v Superman: Dawn of Justice','The Jungle Book','Captain America: Civil War','Star Wars: The Force Awakens']
+    for m in movies:
+        twitter_search(m,api,ia)
